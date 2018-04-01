35dbde10be1a245e0079842fa90cac18d463fb1f
z6shang <z6shang@uwaterloo.ca>
2018-04-01 18:29:05 -0400

    add test for multiple models

--- test_pre_re_acc.py
+++ test_pre_re_acc.py
@@ -1,8 +1,14 @@
 from test_on_model import *
 from neural_networks import *
 from model_evaluation import *
-pre, org = test_on_model_dir('', './DataTest')
-precision = gen_precision(pre, org)
-recall = gen_recall(pre, org)
-accuracy = gen_accuracy(pre, org)
-print ('precision:', precision, 'recall:', recall, 'accuracy:',accuracy)
+
+model_name_pre = './models/graph_embedding_nn_no_'
+model_list = [250, 300, 350, 400, 450, 500, 550, 600]
+
+for m_n in model_list:
+    model_name = model_name_pre + str(m_n)
+    pre, org = test_on_model_dir(model_name, './DataTest')
+    precision = gen_precision(pre, org)
+    recall = gen_recall(pre, org)
+    accuracy = gen_accuracy(pre, org)
+    print ('model_name:', m_n, 'precision:', precision, 'recall:', recall, 'accuracy:',accuracy)
